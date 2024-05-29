6aef4dcc02eec5093aab38c0d5c22fb766d4dc9e
slppills <sunchaehoon2@naver.com>
2024-05-29 14:45:41 +0900

    Update 정수 제곱근 판별.py

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,6 +1,6 @@
 def solution(n):
     a = n ** (1/2) # 소숫점 붙어서 나옴
-    if a == int(a):
+    if a == int(a): # n이 121일 때 a의 값인 11.0과 a를 정수형으로 바꾼 11이 같은지 비교
         return (a+1) ** 2
     else:
         return -1
